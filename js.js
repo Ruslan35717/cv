f140a1bd045de952b957692b824d598f5d3f9894
Ruslan35717 <kalininskyruslan@yandex.ru>
2025-01-26 21:32:09 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('Для тестирования клиента обычно использую DevTools, при проверке Бекэнда - Postman. Чек-листы пишу в Miro, а тест-кейсы - в Test.it. Есть опыт работы с SQL. Опыт PostgeSQL и MongoDB. Svager и Redoc. Rest Api и Soap.Технологии: HTML, CSS, JS, PostgreSQL, REST, JSON.Инструменты: Charles, Test it, DevTools, Swagger, Postman, Kibana,Sentry. Доступы в БД:Metabase,DBeaver   ')
+})
